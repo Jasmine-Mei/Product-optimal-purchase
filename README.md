a3829abb1b694585bcafbe335bbb1118675c6afd
jasmine <1491246034@qq.com>
2019-11-23 16:34:47 +0800

    完成全选全部选按钮功能

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Product-optimal-purchase
 
 For the first time to submit , Implement to commodity details
+
+Complete the shopping cart list display and hide
--- 品优购/js/cart.js
+++ 品优购/js/cart.js
@@ -1,7 +1,9 @@
 $(() => {
+    // 先读取本地存储的数据
     let arr = kits.loadData('cartListData');
+    // 准备一个空字符串,用于后面存储新创建的元素
     let html = '';
-    arr.forEach(function (e) {
+    arr.forEach(e => {
         html += `<div class="item" data-id="${e.pID}">
                     <div class="row">
                         <div class="cell col-1 row">
@@ -46,6 +48,26 @@ $(() => {
         $('.total-of').removeClass('hidden');
         $('.cart-header').removeClass('hidden');
     }
+
+    // 读取本地数据,得到里面的数据是否有勾选
+    let noCkAll = arr.find(e => {
+        return !e.isChecked;
+    })
+    $('.pick-all').prop('checked', !noCkAll);// prop是开关属性 
+
+    // 全选和点选
+    $('.pick-all').on('click', function () {
+        let status = $(this).prop('checked');
+        $('.item-ck').prop('checked', status);
+        $('.pick-all').prop('checked', status);
+        // 先把本地数据里面的所有的数据都勾选
+        arr.forEach(e => {
+            e.isChecked = status;
+        })
+        // 重新存进本地数据
+        kits.saveData('cartListData', arr);
+    })
+
 
 
 
